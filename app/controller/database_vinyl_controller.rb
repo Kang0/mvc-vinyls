69a9954bd6a48955e6bdbcb7741a36dd9a20cc7f
Kevin Kang <kevinkangg@gmail.com>
2019-03-06 22:12:58 +0000

    added edit route and slug routes

--- app/controller/database_vinyl_controller.rb
+++ app/controller/database_vinyl_controller.rb
@@ -30,4 +30,31 @@ class DatabaseVinylController < ApplicationController
     erb :'/DatabaseVinyl/show_database'
   end
 
+  get '/database/:artist_slug' do
+    @dbvinyl = DatabaseVinyl.find_by_artist_slug(params[:artist_slug])
+    erb :'/DatabaseVinyl/show_artist'
+  end
+
+  get '/database/:artist_slug/:album_slug' do
+    @dbvinyl = DatabaseVinyl.find_by_album_slug(params[:album_slug])
+    erb :'/DatabaseVinyl/show_album'
+  end
+
+  get '/database/:artist_slug/:album_slug/edit' do
+    @dbvinyl = DatabaseVinyl.find_by_album_slug(params[:album_slug])
+
+    if session[:user_id] == @dbvinyl.user_id
+      erb :'/DatabaseVinyl/edit'
+    else
+      erb :'/users/error'
+    end
+  end
+
+  patch '/database/:artist_slug/:album_slug/edit' do
+    @dbvinyl = DatabaseVinyl.find_by_album_slug(params[:album_slug])
+    @dbvinyl.update(artist: params[:artist], album_name: params[:album_name], record_label: params[:record_label], year_released: params[:year_released], genre: params[:genre])
+    binding.pry
+    redirect "/database/#{@dbvinyl.slug_artist}/#{@dbvinyl.slug_album}"
+  end
+
 end
